fb719feca95963fa48380dedf2b8443ba794851f
Melusi Ndlovu <melusindlovuc5@gmail.com>
2026-05-20 14:36:56 +0200

    Creating a super user via script

---
+++ backend/createsuperuser.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    )
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
